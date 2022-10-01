5a1dc6924d6ef4c3c7513cf624098fd65714a99c
Onur Alp Bicer <onuralpbicer@gmail.com>
2022-10-01 16:23:26 +1000

    extract logic onto separate file

--- chrome/background.js
+++ chrome/background.js
@@ -2,41 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, { status }) => {
 	if (status === 'complete') {
 		chrome.scripting.executeScript({
 			target: { tabId },
-			func: setPageBackgroundColor,
+			files: ['theater.js'],
 		})
 	}
 })
-
-function setPageBackgroundColor() {
-	const theaterButtonSelector = () =>
-		document.querySelector('[title="Cinema mode (t)"]')
-
-	function main() {
-		/**
-		 * @type HTMLVideoElement
-		 */
-		const video = document.getElementsByTagName('video').item(0)
-
-		// click on the theater mode button after a couple ticks of progress to give the page a change to load
-		let i = 0
-		const handler = () => {
-			i++
-			if (i > 1) {
-				clickOnTheater()
-				video.removeEventListener('progress', handler)
-			}
-		}
-
-		// only do it if you detect the theater button (the button doesn't show up if it's already in theater mode or fullscreen)
-		if (theaterButtonSelector()) video.addEventListener('progress', handler)
-	}
-
-	function clickOnTheater() {
-		const button = theaterButtonSelector()
-		if (button) {
-			button.click()
-		}
-	}
-
-	main()
-}
---
+++ chrome/theater.js
@@ -0,0 +1,31 @@
+const theaterButtonSelector = () =>
+	document.querySelector('[title="Cinema mode (t)"]')
+
+function main() {
+	/**
+	 * @type HTMLVideoElement
+	 */
+	const video = document.getElementsByTagName('video').item(0)
+
+	// click on the theater mode button after a couple ticks of progress to give the page a change to load
+	let i = 0
+	const handler = () => {
+		i++
+		if (i > 1) {
+			clickOnTheater()
+			video.removeEventListener('progress', handler)
+		}
+	}
+
+	// only do it if you detect the theater button (the button doesn't show up if it's already in theater mode or fullscreen)
+	if (theaterButtonSelector()) video.addEventListener('progress', handler)
+}
+
+function clickOnTheater() {
+	const button = theaterButtonSelector()
+	if (button) {
+		button.click()
+	}
+}
+
+main()
